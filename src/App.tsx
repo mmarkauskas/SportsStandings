251cc8f6c43813f0b24636b76ec5668956137c02
Mantas Markauskas <Mantas@MacBook-Air-Kamila.local>
2025-08-06 00:34:05 +0300

    Added a background video

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import footballVideo from '../src/assets/videos/FootballField.mp4';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <video className='video-bg' autoPlay loop muted>
+      <source src={footballVideo}> </source>
+    </video>
   );
-}
+};
 
 export default App;
