4fad480e0566c5f63966daae64e6e74d50f97cbf
Mantas Markauskas <Mantas@MacBook-Air-Kamila.local>
2025-08-06 12:19:44 +0300

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import footballVideo from '../src/assets/videos/FootballField.mp4';
 import './App.css';
 
 const App: React.FC = () => {
-
   return (
-    <video className='video-bg' autoPlay loop muted>
-      <source src={footballVideo}> </source>
-    </video>
+    <div className="relative w-full h-screen overflow-hidden">
+      <video className="absolute top-0 left-0 w-full h-full object-cover -z-10" autoPlay loop muted>
+        <source src={footballVideo} type="video/mp4" />
+      </video>
+    </div>
   );
 };
 
-export default App;
+export default App; 
